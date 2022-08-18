c2ff270cf5209d44b6d7b83cf47d809824b83ac7
Felipe Rosas <felipe.rosas@g.globo>
2022-08-18 20:17:29 -0300

    chore: add faker lib

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const SQLite = require("sqlite3");
 const path = require("path");
 
+const { faker } = require("@faker-js/faker");
+
 const fileDatabase = path.join(__dirname, "db", "database.sqlite");
 
 SQLite.verbose();
@@ -9,8 +11,6 @@ const db = new SQLite.Database(fileDatabase);
 const callback = (err, result) => {
   if (err) {
     console.log(err);
-  } else {
-    console.log("successfully", result);
   }
 };
 
@@ -29,13 +29,15 @@ const createTableUser = async () => {
 async function main() {
   await createTableUser();
 
-  // adicionar usuarios
+  for (let i = 0; i < 100; i++) {
+    const name = faker.name.fullName();
+    const email = faker.internet.email().toLowerCase();
 
-  const sql = `
-    INSERT INTO users (name, email) VALUES ('Felipe', 'contato@eufelipe.com');
-    `;
-
-  db.run(sql, callback);
+    const sql = `
+      INSERT INTO users (name, email) VALUES ('${name}', '${email}');
+      `;
+    db.run(sql, callback);
+  }
 
   db.close();
 }
